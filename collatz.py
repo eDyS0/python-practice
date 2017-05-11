9157aa748c576c37e903a85ede6e99770152722c
eDyS0 <syde.will@gmail.com>
2017-05-11 14:00:36 +0200

    Create collatz.py

---
+++ collatz.py
@@ -0,0 +1,21 @@
+#! python3
+# collatz.py - Collatz Sequence 
+# Usage: python collatz.py <number>
+
+import sys
+def collatz(number):
+    if number % 2 == 1:
+        print(3 * number + 1)
+        return 3 * number + 1
+    elif number % 2 == 0:
+        print(number // 2)
+        return number // 2
+
+if len(sys.argv) != 2 or sys.argv[1].isnumeric() == False:
+    print('Usage: python collatz.py <number>', end = '')
+    exit()
+    
+if int(sys.argv[1]) > 0:
+    nb = int(sys.argv[1])
+    while nb != 1:
+        nb = collatz(nb)
